1fe5311f0eeff5a793dbcf2427f0cef6763f2f5d
jumaalmeida9 <juma_almeida@outlook.com>
2024-06-13 22:17:51 -0300

    tp pratico

---
+++ criar_curso.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Criar Curso</title>
+    </head>
+    <body>
+        <h>Criar Curso</h1>
+        <form method="POST" action= "">
+            Nome:<input type= "text" name="nome">
+
+            <input type="submit" value="Criar Curso">
+        </form>
+    </body>
+</html>
